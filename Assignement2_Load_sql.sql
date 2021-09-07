ffae3712377117db996dc9c1d31f9b6956d472b7
Cadence Ember <cadence@disroot.org>
2021-09-08 00:03:01 +1200

    Add my tables

--- Assignement2_Load_sql.sql
+++ Assignement2_Load_sql.sql
@@ -1,7 +1,7 @@
-DROP TABLE employee_experience; 
-DROP TABLE driving_time_log; 
-DROP TABLE driving_route_log; 
-DROP TABLE roster; 
+DROP TABLE employee_experience;
+DROP TABLE driving_time_log;
+DROP TABLE driving_route_log;
+DROP TABLE roster;
 DROP TABLE employee cascade constraints;
 
 
@@ -14,8 +14,8 @@ CREATE TABLE employee
 
 
 INSERT INTO employee VALUES (12345, 'Fire', 'Sean', 2, 'M');
-INSERT INTO employee VALUES (11234, 'Robertson', 'Luke', 2,'M'); 
-INSERT INTO employee VALUES (12344, 'Gits', 'Samantha', 3,'F'); 
+INSERT INTO employee VALUES (11234, 'Robertson', 'Luke', 2,'M');
+INSERT INTO employee VALUES (12344, 'Gits', 'Samantha', 3,'F');
 INSERT INTO employee VALUES (12234, 'Smith', 'Hyden',1,'M');
 
 
@@ -27,13 +27,13 @@ Years_experience​ INT);
 --CONSTRAINT emp_cnst REFERENCES employee(employee_ID) DISABLE;
 
 INSERT INTO employee_experience VALUES (12345, 1, 2, 20);
-INSERT INTO employee_experience VALUES (11234, 1, 2, 10); 
-INSERT INTO employee_experience VALUES (12344, 1, 2, 15); 
+INSERT INTO employee_experience VALUES (11234, 1, 2, 10);
+INSERT INTO employee_experience VALUES (12344, 1, 2, 15);
 INSERT INTO employee_experience VALUES (12234, 1, 2, 5);
 
 --ALTER TABLE employee ENABLE CONSTRAINT superssn_cnst;
 
---Table: driving_time_log 
+--Table: driving_time_log
 CREATE TABLE driving_time_log
 (Employee_ID INT REFERENCES employee (Employee_ID),
 Log_date DATE,
@@ -72,7 +72,47 @@ INSERT INTO roster VALUES (11234, 'Morning', 'Wed');
 INSERT INTO roster VALUES (11234, 'Morning', 'Thu');
 INSERT INTO roster VALUES (11234, 'Evening', 'Fri');
 
+-- Customer (modeled by Cadence)
 
-Commit;
+DROP TABLE customer CASCADE CONSTRAINTS;
+CREATE TABLE customer (
+  customer_email VARCHAR (320) PRIMARY KEY, -- email addresses can be up to 320 characters long
+  full_name VARCHAR (4000),                 -- names can be any length
+  address VARCHAR (4000),
+  birthdate DATE,
+  card_number CHAR (9) UNIQUE -- NOTE: add reference later
+);
 
+-- Bee card (modeled by Cadence)
 
+DROP TABLE bee_card CASCADE CONSTRAINTS;
+CREATE TABLE bee_card (
+  card_number CHAR (9) PRIMARY KEY, -- bee card ids are exactly 9 digits long
+  balance DECIMAL (6, 2) NOT NULL, -- bee card maximum balance is $2000, so this fits all the digits
+  customer_email VARCHAR (320) NOT NULL UNIQUE, -- email addresses can be up to 320 characters long
+  CONSTRAINT bee_card_balance_min CHECK (balance >= 0)
+);
+
+-- Trip (modeled by Cadence)
+-- trip is a weak entity and has no primary key
+DROP TABLE trip CASCADE CONSTRAINTS;
+CREATE TABLE trip (
+  trip_started DATE NOT NULL,
+  trip_ended DATE NOT NULL,
+  fare_charged DECIMAL (4, 2) NOT NULL,
+  card_number CHAR (9) -- may be null because the trip may have been paid in cash
+  bus_number_plate VARCHAR (6) NOT NULL, -- TODO: need to add reference
+  employee_id INT NOT NULL,
+  starts_at_stop INT NOT NULL, -- TODO: need to add reference
+  ends_at_stop INT NOT NULL, -- TODO: need to add reference
+  route_id INT NOT NULL,, -- TODO: need to add reference
+);
+
+-- Foreign key constraints for Cadence's tables
+
+ALTER TABLE customer ADD FOREIGN KEY (card_number) REFERENCES bee_card (card_number);
+ALTER TABLE bee_card ADD FOREIGN KEY (customer_email) REFERENCES customer (customer_email) ON DELETE CASCADE;
+ALTER TABLE trip ADD FOREIGN KEY (card_number) REFERENCES bee_card (card_number) ON DELETE CASCADE;
+ALTER TABLE trip ADD FOREIGN KEY (employee_id) REFERENCES employee (Employee_ID) ON DELETE CASCADE;
+
+COMMIT;
